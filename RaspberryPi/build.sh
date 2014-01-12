108dfc5cf93bb1e7b75559dd5eb5922545c90ae5
shawn <shawn@uugear.com>
2014-01-12 16:53:55 +0100

    Building script

---
+++ RaspberryPi/build.sh
@@ -0,0 +1,33 @@
+CURRENT_DIR=`readlink -f .`
+
+echo "CURRENT_DIR = " $CURRENT_DIR
+
+TARGET_DIR=$CURRENT_DIR/bin
+
+rm -rf $TARGET_DIR
+
+mkdir $TARGET_DIR
+
+echo "TARGET_DIR = " $TARGET_DIR
+
+cd src
+
+echo "Building src..."
+
+cc -o $TARGET_DIR/UUGearDaemon UUGearDaemon.c serial.c -lrt
+cc -o $TARGET_DIR/UUGear.o -c -Wall -Werror -fPIC UUGear.c 
+cc -shared -o $TARGET_DIR/libUUGear.so $TARGET_DIR/UUGear.o -lrt
+
+cc -o $TARGET_DIR/lsuu lsuu.c serial.c
+
+echo "Building example..."
+
+EXAMPLE_DIR=$CURRENT_DIR/example
+
+cd $EXAMPLE_DIR
+
+cc -L$TARGET_DIR -Wall test.c -o $TARGET_DIR/test -lUUGear -lrt
+
+cd $CURRENT_DIR
+
+echo "Build End"
