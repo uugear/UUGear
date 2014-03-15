4e89c57798be1637faf7156acb9a9650546e7767
shawn <shawn@uugear.com>
2014-03-15 15:47:18 +0100

    Update the DHT11 example for Python

--- RaspberryPi/example/python/ReadDHT11.py
+++ RaspberryPi/example/python/ReadDHT11.py
@@ -1,15 +1,19 @@
 from time import sleep
 from UUGear import *
+from time import gmtime, strftime
 
 UUGearDevice.setShowLogs(0)
 
 device = UUGearDevice('UUGear-Arduino-7853-2668')
 
 if device.isValid():
-	value = device.readDHT11(4)
-	humidity = (value >> 8)
-	temperature = (value & 255)
-	print 'Humidity:', humidity, '%  Temperature:', temperature, 'C'
+	for i in range(200):
+		value = device.readDHT11(4)
+		humidity = (value >> 8)
+		temperature = (value & 255)
+		print strftime("Time: %H:%M:%S", gmtime())
+		print 'H:', humidity, '%  T:', temperature, 'C'
+		sleep(1)
 	device.detach()
 	device.stopDaemon()
 else:
